5a64f4a3ad318c4c546b8b7b6e29391e28a90a4e
Ubuntu <ubuntu@ip-10-0-1-188.us-east-2.compute.internal>
2023-02-24 01:49:18 +0000

    First commit

--- app.py
+++ app.py
@@ -8,50 +8,6 @@ def index():
 
 @app.route('/process',methods= ['POST'])
 
-# def process():
-#   weight_in_kg  = float(request.form['weight_in_kg'])
-#   height_in_foot_and_inches = float(request.form['height_in_foot_and_inches'])
-#   height_in_meters=0.304* float(height_in_foot_and_inches)
-#   output = str(weight_in_kg / (height_in_meters*height_in_meters))
-#   bmi =float(output)
-
-#   if bmi <=18.5:
-#     return jsonify({'output':'You are Under weight as your bmi is: ' + output})
-#   elif (bmi >=18.5) and (bmi <=24.9):
-#     return jsonify({'output':'Perfect!You have normal weight and your bmi is : '+ output})
-#   elif bmi >=25 and bmi <=29.9:
-#    return jsonify({'output':'You are Overweight as your bmi is: ' + output})
-#   elif bmi>=30:
-#    return jsonify({'output':'You are highly obese as your bmi is: ' + output})
-#   else :
-#    return jsonify({'output':'and your bmi is : '+ output})
-
-#   return jsonify({'error' : 'Missing data!'})
-
-# def process():
-  
-#   q  = request.form['asked_question']
-  
-#   # weight_in_kg  = float(request.form['weight_in_kg'])
-#   # height_in_foot_and_inches = float(request.form['height_in_foot_and_inches'])
-#   # height_in_meters=0.304* float(height_in_foot_and_inches)
-#   # output = str(weight_in_kg / (height_in_meters*height_in_meters))
-#   # bmi =float(output)
-
-#   if bmi <=18.5:
-#     return jsonify({'output':'You are Under weight as your bmi is: ' + output})
-#   elif (bmi >=18.5) and (bmi <=24.9):
-#     return jsonify({'output':'Perfect!You have normal weight and your bmi is : '+ output})
-#   elif bmi >=25 and bmi <=29.9:
-#    return jsonify({'output':'You are Overweight as your bmi is: ' + output})
-#   elif bmi>=30:
-#    return jsonify({'output':'You are highly obese as your bmi is: ' + output})
-#   else :
-#    return jsonify({'output':'and your bmi is : '+ output})
-
-#   return jsonify({'error' : 'Missing data!'})
-
-
 def process():
 
     q  = request.form['asked_question']
